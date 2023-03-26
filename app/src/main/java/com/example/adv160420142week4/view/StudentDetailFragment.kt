627ec04b913ec40f781211812fbe0cb4642f0ce2
alfathsyah <alfathsyah10@gmail.com>
2023-03-26 20:51:39 +0700

    init

--- app/src/main/java/com/example/adv160420142week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/adv160420142week4/view/StudentDetailFragment.kt
@@ -5,30 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.adv160420142week4.R
+import com.example.adv160420142week4.viewmodel.DetailViewModel
+import com.google.android.material.textfield.TextInputEditText
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudentDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var detailViewModel: DetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +22,19 @@ class StudentDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        detailViewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        detailViewModel.fetch()
+        val txtID = view.findViewById<TextInputEditText>(R.id.txtID)
+        val txtName = view.findViewById<TextInputEditText>(R.id.txtName)
+        val txtBod = view.findViewById<TextInputEditText>(R.id.txtBod)
+        val txtPhone = view.findViewById<TextInputEditText>(R.id.txtPhone)
+        detailViewModel.studentLD.observe(viewLifecycleOwner){studentDetail ->
+            txtID.setText(studentDetail.id.toString())
+            txtName.setText(studentDetail.name.toString())
+            txtBod.setText(studentDetail.dob.toString())
+            txtPhone.setText(studentDetail.phone.toString())
+        }
     }
 }
